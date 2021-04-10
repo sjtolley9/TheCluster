061f62559e19194d90ae2c3b3c9c4521c23fc75c
Sterling Tolley <sterlingj.tolley@gmail.com>
2021-04-10 09:24:23 -0600

    I'm an idiot and added the secret key

--- cluster/cluster/settings.py
+++ cluster/cluster/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q6f^p2(e=p@r_@8+oc7p0x5fd87khhas@_wgm3r*azly3ddg_n'
+SECRET_KEY = secret.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
