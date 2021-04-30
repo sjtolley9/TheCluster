ed65cad1e3d88ed2d2c1e66f613b55c6a252e37a
Sterling Tolley <sterlingj.tolley@gmail.com>
2021-04-30 09:09:38 -0600

    Added an authentication system

--- cluster/portal/urls.py
+++ cluster/portal/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new', views.create_job, name='create'),
+    path('new/', views.create_job, name='create'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- cluster/portal/views.py
+++ cluster/portal/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from .models import UploadJobForm
+from .forms import LoginForm
 
+def login_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/new/')
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            return auth_view(request)
+    else:
+        form = LoginForm()
+
+    return render(request, 'portal/login.html', {'form': form})
+
+def auth_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect('/new')
+    else:
+        pass
+        return HttpResponseRedirect('/login/')
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+@login_required(login_url='/login/')
 def create_job(request):
     template = 'portal/upload.html'
     if request.method == 'POST':
@@ -16,7 +48,10 @@ def create_job(request):
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to the Rockslide Portal")
+    if not request.user.is_authenticated:
+        return HttpResponse("Welcome to the Rockslide Portal")
+    else:
+        return HttpResponse(f"Welcome to the Rockslide Portal, {request.user.username}")
 
 def top(request):
     return redirect('/portal/')
